40048f843c31324f9e729679e72b1af76d2a0be2
boredcoder411 <taamarcos@gmail.com>
2024-08-03 18:14:25 +0200

    Push, not even sure it works

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use warp::Filter;
-use std::collections::HashMap;
+use warp::{Filter, Reply};
+use std::{collections::HashMap, str::FromStr};
 use serde::Deserialize;
 use serde_json;
 use std::fs;
+use reqwest::Client;
 
 #[derive(Deserialize)]
 struct ConfigData {
@@ -17,18 +18,39 @@ async fn main() {
     let port = config_deser.port;
     let domain_map = config_deser.domain_map;
 
+    // Move domain_map inside the filter to avoid ownership issues
+    let domain_map_filter = warp::any().map(move || domain_map.clone());
+
     let hi = warp::path("hello")
         .and(warp::path::param())
         .and(warp::header("user-agent"))
         .and(warp::header("host"))
-        .map(move |param: String, agent: String, host: String| {
-            println!("param: {}, agent: {}, host: {}", param, agent, host);
-            let host = host.split(":").collect::<Vec<&str>>()[0].to_string();
-            domain_map.get(&host).unwrap_or(&"No domain found".to_string()).to_string()
-        });
-    
+        .and(domain_map_filter)
+        .and_then(
+            |param: String, agent: String, host: String, domain_map: HashMap<String, String>| async move {
+                println!("param: {}, agent: {}, host: {}", param, agent, host);
+                let host = host.split(":").collect::<Vec<&str>>()[0].to_string();
+                if let Some(target) = domain_map.get(&host) {
+                    // forward request
+                    let client = Client::new();
+                    let url = format!("http://{}/{}", target, param);
+                    let request = client.get(&url).header("User-Agent", agent.clone()).build().unwrap();
+                    match client.execute(request).await {
+                        Ok(response) => {
+                            let status = response.status();
+                            let status_str = status.as_str();
+                            let body = response.text().await.unwrap_or_else(|_| "Error".into());
+                            Ok::<_, warp::Rejection>(warp::reply::with_status(body, warp::http::StatusCode::from_str(status_str).unwrap()).into_response())
+                        },
+                        Err(_) => Ok(warp::reply::with_status::<&'static str>("Error".into(), warp::http::StatusCode::INTERNAL_SERVER_ERROR).into_response()),
+                    }
+                } else {
+                    Ok(warp::reply::with_status(warp::reply::html("Not Found"), warp::http::StatusCode::NOT_FOUND).into_response())
+                }
+            }
+        );
 
     warp::serve(hi)
-        .run(([127, 0, 0, 1], port.into()))
+        .run(([127, 0, 0, 1], port))
         .await;
 }
