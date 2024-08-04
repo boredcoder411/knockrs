18a876feeedb8e049236c1f5f45c332c62fddb8b
boredcoder411 <taamarcos@gmail.com>
2024-08-04 18:32:39 +0200

    Added extensive logging

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use serde_json;
 use std::fs;
 use reqwest;
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 struct ConfigData {
     port: u16,
     domain_map: HashMap<String, String>,
@@ -14,12 +14,17 @@ struct ConfigData {
 #[tokio::main]
 async fn main() {
     // Read the configuration file
+    println!("Reading configuration file...");
     let config = fs::read_to_string("config.json").expect("Unable to read file");
+    println!("Config file content: {}", config);
     let config_deser: ConfigData = serde_json::from_str(&config).expect("Unable to deserialize");
+    println!("Deserialized config: {:?}", config_deser);
     let port = config_deser.port;
     let domain_map = config_deser.domain_map.clone();
+    println!("Port: {}, Domain map: {:?}", port, domain_map);
 
     // Define the warp filter
+    println!("Defining warp filter...");
     let domain_map_filter = warp::any().map(move || domain_map.clone());
     let hi = warp::path::full()
         .and(warp::header("user-agent"))
@@ -28,6 +33,7 @@ async fn main() {
         .and_then(handle_request);
 
     // Run the warp server
+    println!("Starting warp server on port {}...", port);
     warp::serve(hi)
         .run(([127, 0, 0, 1], port))
         .await;
@@ -40,14 +46,21 @@ async fn handle_request(
     host: String,
     domain_map: HashMap<String, String>,
 ) -> Result<impl warp::Reply, warp::Rejection> {
+    println!("Received request - Path: {}, Agent: {}, Host: {}", path.as_str(), agent, host);
     let host = host.split(":").collect::<Vec<&str>>()[0].to_string();
-    println!("Path: {}, agent: {}, host: {}", path.as_str(), agent, host);
+    println!("Processed host: {}", host);
 
     // Get the forwarding address from the domain map
-    if let Some(forward_address) = format!("http://localhost:{}{}", domain_map.get(&host).unwrap_or(&"".to_string()), path.as_str()).parse::<String>().ok() {
+    if let Some(forward_address) = domain_map.get(&host) {
+        let full_forward_address = format!("http://localhost:{}{}", forward_address, path.as_str());
+        println!("Forwarding address: {}", full_forward_address);
+        
         // Forward the request and return the response
-        match forward_request(&forward_address).await {
-            Ok(response_body) => Ok(warp::reply::Response::new(response_body.into())),
+        match forward_request(&full_forward_address).await {
+            Ok(response_body) => {
+                println!("Forward request successful, response body: {}", response_body);
+                Ok(warp::reply::Response::new(response_body.into()))
+            },
             Err(err) => {
                 eprintln!("Error forwarding request: {:?}", err);
                 Ok(warp::reply::Response::new("Failed to forward request".into()))
@@ -55,21 +68,27 @@ async fn handle_request(
         }
     } else {
         // Return an error response if no domain is found
+        println!("No domain found for host: {}", host);
         Ok(warp::reply::Response::new("No domain found".into()))
     }
 }
 
 // The function to forward requests
 async fn forward_request(forward_address: &str) -> Result<String, reqwest::Error> {
+    println!("Forwarding request to address: {}", forward_address);
+
     // Create a new reqwest client
     let client = reqwest::Client::new();
 
     // Make a GET request to the forwarding address
     let response = client.get(forward_address).send().await?;
+    println!("Received response with status: {}", response.status());
 
     // Read the response body as a string
     let body = response.text().await?;
+    println!("Response body: {}", body);
 
     // Return the response body
     Ok(body)
 }
+
